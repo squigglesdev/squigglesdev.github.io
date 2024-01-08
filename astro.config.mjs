5ccc21e97787c64637d94812f6c9b44b7936ac9f
Squiggles <luke.saynor@gmail.com>
2024-01-08 07:31:28 +0000

    halfway through blog 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,34 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
-export default defineConfig({});
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [
+      remarkMath
+    ],
+    rehypePlugins: [
+      [rehypeKatex, {
+        macros: {
+          '\\E': '\\mathbb{E}',
+          '\\C': '\\mathbb{C}',
+          '\\R': '\\mathbb{R}',
+          '\\N': '\\mathbb{N}',
+          '\\Q': '\\mathbb{Q}',
+          '\\bigO': '\\mathcal{O}',
+          '\\abs': '|#1|',
+          '\\set': '\\{ #1 \\}',
+          '\\indep': "{\\perp\\mkern-9.5mu\\perp}",
+          '\\nindep': "{\\not\\!\\perp\\!\\!\\!\\perp}",
+          "\\latex": "\\LaTeX",
+          "\\katex": "\\KaTeX",
+        },
+      }]
+    ]
+  }
+});
